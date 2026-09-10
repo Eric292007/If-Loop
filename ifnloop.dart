7149c77cc3fcea201bea4d0b51bf1350447563c2
Eric_V <110581181+Eric292007@users.noreply.github.com>
2026-09-10 19:43:14 +0700

    2

--- ifnloop.dart
+++ ifnloop.dart
@@ -1,3 +1,103 @@
-void main(){
-  print("Hello, World!");
+void main() {
+  // if
+
+  // int nilai = 86;
+
+  // if (nilai > 85) {
+  //   print('nilai di atas 85');
+  // }
+
+  // if (nilai > 85) {
+  //   print('nilai di atas 85');
+  // } else {
+  //   print('nilai di bawah 85');
+  // }
+
+  // if (nilai > 85) {
+  //   print('nilai di atas 85');
+  // }
+
+  // if (nilai < 85) {
+  //   print('nilai di bawah 85');
+  // }
+
+  //Switch case
+  // var nilai = 'D';
+  // switch (nilai) {
+  //   case 'A':
+  //     print('Nilai di atas Adalah A');
+  //     break;
+  //   case 'B':
+  //     print('Nilai di atas Adalah B');
+  //     break;
+  //   case 'C':
+  //     print('Nilai di atas Adalah C');
+  //     break;
+  //   default:
+  //     print('Nilai diluar piihan');
+  // }
+
+  //for
+  //for in
+  //foreach
+  //while
+  //dowhile
+
+  // for (var i = 0; i < 10; i++) {
+  //   print('nilai $i');
+  // }
+
+  // List datamhs =['Deni', 'Windy', 'Sindy', 'Micheal', 'Jonathan'];
+  // for (var i = 0; i < datamhs.length; i++) {
+  //   print('nama mhs = ${datamhs[i]}');
+  // if (i == 0) {
+  //   print('Ini Deni');
+  //   }
+  // }
+
+  //for in
+  // List datamhs = ['Deni', 'Windy', 'Sindy', 'Micheal', 'Jonathan'];
+
+  // for (var element in datamhs) {
+  //   if (element == 'Deni') {
+  //     print('Ini Deni');
+  //   }
+  //   print(element);
+  // }
+
+  //foreach
+
+  // List datamhs = ['Deni', 'Windy', 'Sindy', 'Micheal', 'Jonathan'];
+
+  // datamhs.forEach((a) {
+  //   // print(a);
+  //   if (a == 'Deni') {
+  //     print(a);
+  //   }
+  // });
+
+  //while
+  // var nilai = 0;
+
+  // while (nilai < 5) {
+  //   print(nilai);
+  //   nilai++;
+  // }
+
+  // List datamhs = ['Deni', 'Windy', 'Sindy', 'Micheal', 'Jonathan'];
+  // var index = 0;
+
+  // while (index < datamhs.length) {
+  //   print(datamhs[index]);
+  //   index++;
+  // }
+
+  //Do While
+  List datamhs = ['Deni', 'Windy', 'Sindy', 'Micheal', 'Jonathan'];
+  var index = 0;
+
+  do {
+    print(datamhs[index]);
+    index++;
+  } while (index < datamhs.length);
 }
